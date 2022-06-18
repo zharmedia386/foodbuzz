6903ab2e6d42df37b82d558263dff84453cb4f1f
M Azhar Alauddin <80077273+zharmedia386@users.noreply.github.com>
2022-06-18 16:45:39 +0700

    Heroku Pushed 10

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ import datetime
 from flask import Flask, render_template
 from get_data import get_data
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def dashboard():
     numbers_users, numbers_orders, numbers_items, status_users, total_profit, item_ordered, users_list, image_users = get_data()
     return render_template('index.html', numbers_users=numbers_users, numbers_orders=numbers_orders, numbers_items=numbers_items, status_users=status_users, total_profit=total_profit, item_ordered=item_ordered, users=users_list, image_users=image_users)
 
 if __name__ == "__main__":
-    # app.run(port=5000)
+    # application.run(port=5000)
 
     # for heroku
-    app.run(debug=True)
+    application.run(debug=True)
